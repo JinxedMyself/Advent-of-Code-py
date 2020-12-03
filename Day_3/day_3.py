0efd50252a8b2f24a549284a2741cb38bd513593
BrianBatenburg <brian.vanbatenburg@student.hu.nl>
2020-12-03 14:14:57 +0100

    Update day_3.py
    Day 3 finished

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -1,6 +1,7 @@
 # i dont like where this is going
 # it involves map n stuff
 import math
+from functools import reduce
 
 map_file = open(r"Day_3\trees_map.txt", "r")
 # for some reason this works with raw and i dont like this
@@ -9,7 +10,7 @@ x_pos = 0
 y_pos = 0
 moves = [[1,1], [3,1], [5,1], [7,1], [1,2]]
 trees = 0
-parttwo_trees = [[0], [0], [0], [0], [0]]
+global_trees_list = []
 # [right_move, down_move]
 map_lines = []
 
@@ -17,25 +18,29 @@ for line in map_file:
     map_lines.append(line.strip())
 
 # part 1
-# for x in range(math.floor(len(map_lines)/moves[1][1])):
-#     if (map_lines[y_pos])[x_pos] == "#":
-#         trees += 1
-#     x_pos += moves[1][0]
-#     y_pos += moves[1][1]
-#     if x_pos >= len(map_lines[x]):
-#         x_pos = x_pos-len(map_lines[x])
+for x in range(math.floor(len(map_lines)/moves[1][1])):
+    if (map_lines[y_pos])[x_pos] == "#":
+        trees += 1
+    x_pos += moves[1][0]
+    y_pos += moves[1][1]
+    if x_pos >= len(map_lines[x]):
+        x_pos = x_pos-len(map_lines[x])
 
-# print(f"Trees: {trees}")
+print(f"Trees Part 1: {trees}")
 
+# part 2
 for z in range(len(moves)):
     x_pos = 0
     y_pos = 0
+    local_trees = 0
     for x in range(math.floor(len(map_lines)/moves[z][1])):
         if (map_lines[y_pos])[x_pos] == "#":
-            parttwo_trees[z] += 1
+            local_trees += 1
         x_pos += moves[z][0]
         y_pos += moves[z][1]
         if x_pos >= len(map_lines[x]):
             x_pos = x_pos-len(map_lines[x])
+    global_trees_list.append(local_trees)
 
-print(f"Trees: {parttwo_trees}")
+print(f"Trees Part 2: {reduce(lambda x, y: x*y, global_trees_list)}")
+# definitely stole/borrowed this because i have no idea how to use lambda properly
