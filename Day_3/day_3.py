fa240e715d2743b4996c46e7bb27ad683a69d356
BrianBatenburg <brian.vanbatenburg@student.hu.nl>
2020-12-03 13:57:15 +0100

    Update day_3.py

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -1,4 +1,3 @@
-# day 3
 # i dont like where this is going
 # it involves map n stuff
 import math
@@ -8,20 +7,35 @@ map_file = open(r"Day_3\trees_map.txt", "r")
 trees = 0
 x_pos = 0
 y_pos = 0
-right_move = 3
-down_move = 1
+moves = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+trees = 0
+parttwo_trees = [[0], [0], [0], [0], [0]]
+# [right_move, down_move]
 map_lines = []
 
 for line in map_file:
     map_lines.append(line.strip())
 
 # part 1
-for x in range(math.floor(len(map_lines)/down_move)):
-    if (map_lines[y_pos])[x_pos] == "#":
-        trees += 1
-    x_pos += right_move
-    y_pos += down_move
-    if x_pos >= len(map_lines[x]):
-        x_pos = x_pos-len(map_lines[x])
+# for x in range(math.floor(len(map_lines)/moves[1][1])):
+#     if (map_lines[y_pos])[x_pos] == "#":
+#         trees += 1
+#     x_pos += moves[1][0]
+#     y_pos += moves[1][1]
+#     if x_pos >= len(map_lines[x]):
+#         x_pos = x_pos-len(map_lines[x])
 
-print(f"Trees: {trees}")
+# print(f"Trees: {trees}")
+
+for z in range(len(moves)):
+    x_pos = 0
+    y_pos = 0
+    for x in range(math.floor(len(map_lines)/moves[z][1])):
+        if (map_lines[y_pos])[x_pos] == "#":
+            parttwo_trees[z] += 1
+        x_pos += moves[z][0]
+        y_pos += moves[z][1]
+        if x_pos >= len(map_lines[x]):
+            x_pos = x_pos-len(map_lines[x])
+
+print(f"Trees: {parttwo_trees}")
