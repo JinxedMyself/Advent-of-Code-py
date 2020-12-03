127ef0b443c9e8d009f2da4ac3983515076ea261
BrianBatenburg <brian.vanbatenburg@student.hu.nl>
2020-12-03 13:15:27 +0100

    Update day_3.py

--- Day 3/day_3.py
+++ Day 3/day_3.py
@@ -1,3 +1,15 @@
 # day 3
 # i dont like where this is going
 # it involves map n stuff
+import math
+
+map_file = open("map.txt", "r")
+trees = 0
+x_pos = 0
+y_pos = 0
+right_move = 3
+down_move = 1
+map_lines = []
+
+for line in map_file:
+    map_lines.append(line.strip())
